ce82a74fc4ea3544a124eb8900f78a21cace98ae
Ryan Miller <miller2300@comcast.net>
2012-04-10 10:24:25 -0400

    traingle is passing

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,17 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  # add a, b, c to array, compress array and get count
+  sides = []
+  sides << a << b << c
+  unique_sides = sides.uniq.length
+  if unique_sides == 1
+    return :equilateral
+  elsif unique_sides == 2
+    return :isosceles
+  else 
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
